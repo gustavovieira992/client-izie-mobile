0f34082040caf8592fbc76f03079257d7d7ddd2d
gustavovieira <gustavovieira992@gmail.com>
2017-05-17 05:54:52 -0300

    [fix] upload de imagem
    # Conflicts:
    #	bower.json
    #	www/js/app.js

--- www/js/constants.js
+++ www/js/constants.js
@@ -1,7 +1,7 @@
 angular.module('constants', [])
 
 .constant('UrlsConst', (function () {
-  var baseUrl = 'http://localhost:8000';
+  var baseUrl = 'http://192.168.0.13:8000';
 
   return {
     'baseUrl': baseUrl
